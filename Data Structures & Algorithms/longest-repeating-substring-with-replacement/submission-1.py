169eadbd6a80be610169f0b103a8228a87930da6
ReFac <fanjiaruichina@gmail.com>
2026-09-20 21:10:39 +0800

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        count ={}
+        highest = 0
+        
+        for right , ch in enumerate(s):
+            count[ch] = count.get(ch,0) + 1
+            highest = max(count[ch], highest)
+
+            if (right - left + 1) > (highest + k):
+                count[s[left]] -=1
+                left +=1
+        
+        return min(highest+k,len(s))
