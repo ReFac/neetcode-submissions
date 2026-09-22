f75f7482d2aeb85836716c7541ea69a1f86f989a
ReFac <fanjiaruichina@gmail.com>
2026-09-22 11:09:29 +0800

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        count = Counter(t)
+        left = 0
+        check = 0
+        l_c = len(count)
+        smallest = float("inf")
+        result = (-1,-1)
+        for i,x in enumerate(s):
+            if x in count:
+                count[x] -= 1
+                if count[x] == 0:
+                    check +=1
+            while check == l_c:
+                if (i-left+1) < smallest:
+                    smallest = (i-left+1)
+                    result = (left,i+1)
+                if s[left] in count:
+                    count[s[left]] += 1
+                    if count[s[left]] == 1:
+                        check -=1
+                left +=1
+        return s[result[0]:result[1]] if result[0]!= -1 else "" 
+                
+        
